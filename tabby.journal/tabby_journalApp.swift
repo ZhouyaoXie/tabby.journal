bebc08600a758f6542d3aae48d39e5fa6023fa65
Zhouyao Xie <xiezhouyao99@outlook.com>
2025-05-07 22:13:35 -0700

    Initial Commit

---
+++ tabby.journal/tabby_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabby_journalApp.swift
+//  tabby.journal
+//
+//  Created by Joy Xie on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct tabby_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tabby.journalTests/tabby_journalTests.swift
@@ -0,0 +1,16 @@
+//
+//  tabby_journalTests.swift
+//  tabby.journalTests
+//
+//  Created by Joy Xie on 5/7/25.
+//
+
+import Testing
+
+struct tabby_journalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
